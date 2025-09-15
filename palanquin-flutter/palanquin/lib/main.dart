a95b381e8a806b023d5f8e744ed4381ae4000eb5
Arunabh Das <arunabhdas@gmail.com>
2025-09-15 14:09:19 -0400

    Added MyApp StatelessWidget

---
+++ palanquin-flutter/palanquin/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyAppp extends StatelessWidget {
+  const MyAppp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
